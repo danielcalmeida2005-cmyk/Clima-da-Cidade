a85a128060b4c94e8f921dc649087b2578b7f357
daniel carvalho <daniel.calmeida2005@gmail.com>
2026-07-25 23:38:01 -0300

    adiciona interface desktop e define tipografia

--- public/logical.js
+++ public/logical.js
@@ -25,7 +25,7 @@ async function Clima(cidade) {
 
         nomeCidade.innerHTML = dados.clima.name;
         graus.innerHTML = `${Math.round(dados.clima.main.temp)}°C`;
-        ProbalidadeDechuva.innerHTML = `Chances de chuva ${dados.previsao.list[0].pop * 100}%`;
+        ProbalidadeDechuva.innerHTML = `Chance of Rain ${dados.previsao.list[0].pop * 100}%`;
 
 
 
@@ -64,7 +64,7 @@ async function Clima(cidade) {
         let Uv = document.querySelector(".Uv h4")
 
 
-        SensacaoTermica.innerHTML = Math.round(dados.clima.main.feels_like);
+        SensacaoTermica.innerHTML = `${Math.round(dados.clima.main.feels_like)} °`;
         PorcetagemDeChuva.innerHTML = `${dados.previsao.list[0].pop * 100}%`;
 
         velocidadeDoVento.innerHTML = `${Math.round(dados.clima.wind.speed)} Km/h`
