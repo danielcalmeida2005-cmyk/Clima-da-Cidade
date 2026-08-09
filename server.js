5226917fe5cafde01e02fcadaa62a49e61ca44d8
daniel carvalho <daniel.calmeida2005@gmail.com>
2026-08-08 21:36:56 -0300

    teste

--- server.js
+++ server.js
@@ -1,9 +1,7 @@
 
 const express = require("express");
 require("dotenv").config();
-import path from ("path")
 
-app.use(express.static(path.join(__dirname,"public")))
 
 const apiKey = process.env.OPENWEATHER_KEY;
 
