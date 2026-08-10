eec31be33acbd8e22757539bcff7da23296d988e
daniel carvalho <daniel.calmeida2005@gmail.com>
2026-08-10 16:58:18 -0300

    corrige configuração da porta no servidor

--- server.js
+++ server.js
@@ -132,6 +132,6 @@ app.post("/login", async (req, res) => {
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
- console.log(`Servidor rodando na porta ${PORT}`);
+app.listen(PORT, "0.0.0.0", () => {
+    console.log(`Servidor rodando na porta ${PORT}`);
 });
